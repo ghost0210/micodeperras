2839b886778df190fe73fc1a8b590e8fa3c91fd8
ghost0210 <103672354+ghost0210@users.noreply.github.com>
2022-11-19 23:57:35 -0500

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from pydownloader.downloader import Downloader
 import shorturl
 import xdlink
 
-tl_admin_users = ['Ghosthell0210',] #Poner aqui los user con acceso permanente
+tl_admin_users = ['Ghosthell0210','az9az999999',] #Poner aqui los user con acceso permanente
 godlist = ['az9az999999'] #Poner aqui los admin 
 
 async def get_root(username):
