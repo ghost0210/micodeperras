7e786f47276cb91e4c6239c6b975c078f388fe34
ghost0210 <103672354+ghost0210@users.noreply.github.com>
2022-11-19 23:12:18 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 import ProxyCloud
 
-BOT_TOKEN = '5636840268:AApgv5m2eNFbM6dFZo0QclXQUwQ' #Aqui va el token del bot
+BOT_TOKEN = '5841971497:AAHZJCzjA8LwN72fkqcewq5YC7dYMRdDoOk' #Aqui va el token del bot
 API_ID =  12168140 #Tu api id de telegram
 API_HASH = '3504ce0eddb7dff4288d05d5e3dc5e4c' #Tu api id de telegram
 SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
-ACCES_USERS = os.environ.get('tl_admin_user','raydel0307').split(';')
+ACCES_USERS = os.environ.get('tl_admin_user','Ghosthell0210').split(';')
 
 static_proxy = '' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
 PROXY = ProxyCloud.parse(static_proxy)
