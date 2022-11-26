8292509633079149fc8cb29638809e7abc83dab8
ghost0210 <cristianghost4@gmail.com>
2022-11-26 10:22:13 -0500

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ SPLIT_FILE = 1024 * 1024 * int(os.environ.get('split_file','99'))
 ROOT_PATH = 'root/'
 ACCES_USERS = os.environ.get('tl_admin_user','Ghosthell0210').split(';')
 
-static_proxy = 'http://KGGJJJYIJFLIFEYDJFLEKHYGCHDHRHHKCHHGCE' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
+static_proxy      =      'socks5://KIDHKHYEJHJHGDYGJIGIGEYHIDIIRIGILGGJKG' #agrega si kieres tener un proxy statico Con @raydel0307 si kieres comprar un proxy
 PROXY = ProxyCloud.parse(static_proxy)
 
 if PROXY:
